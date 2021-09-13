ffd14838eefb394542324db1ef030827b8c60a1e
Mamednoor <mamed.gohabur@gmail.com>
2021-09-13 19:46:28 +0200

    adding linter, moving function in user.js

--- routes/users.js
+++ routes/users.js
@@ -1,99 +1,104 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 const {
-  insertUser,
-  getUserById,
-  getUserByEmail,
-} = require("../model/User.model");
-const { hashPassword, comparePassword } = require("../services/bcrypt");
+	insertUser,
+	getUserById,
+	getUserByEmail,
+} = require('../model/User.model')
+const { hashPassword, comparePassword } = require('../services/bcrypt')
 const {
-  createAccessToken,
-  createRefreshToken,
-} = require("../services/setToken");
+	createAccessToken,
+	createRefreshToken,
+} = require('../services/setToken')
 
-const { checkToken } = require("../services/checkToken");
+const { checkToken } = require('../services/checkToken')
 
-router.all("/", (req, res, next) => {
-  next();
-});
-
-// profil utilisateur
-router.get("/user", checkToken, async (req, res) => {
-  const _id = req.userId;
-  const userProfil = await getUserById(_id);
-  res.json({ user: userProfil });
-});
+router.all('/', (req, res, next) => {
+	next()
+})
 
 // création d'un utilisateur
-router.post("/", async (req, res) => {
-  const { firstname, lastname, company, address, phone, email, password } =
-    req.body;
-  try {
-    // hash password with bcrypt
-    const hashedPwd = await hashPassword(password);
+router.post('/', async (req, res) => {
+	const { firstname, lastname, company, address, phone, email, password } =
+		req.body
+	try {
+		// hash password with bcrypt
+		const hashedPwd = await hashPassword(password)
 
-    const newUser = {
-      firstname,
-      lastname,
-      company,
-      address,
-      phone,
-      email,
-      password: hashedPwd,
-    };
+		const newUser = {
+			firstname,
+			lastname,
+			company,
+			address,
+			phone,
+			email,
+			password: hashedPwd,
+		}
 
-    const result = await insertUser(newUser);
+		const result = await insertUser(newUser)
 
-    // console.log("création de l'utilisateur réussis", result);
-    res
-      .status(201)
-      .json({ message: "Un nouvelle utilisateur a été crée", result });
-  } catch (error) {
-    // console.log("erreur lors de la création de l'utilisateur", error);
-    res.status(400).json({ message: error.message });
-  }
-});
+		// console.log("création de l'utilisateur réussis", result);
+		res.status(201).json({
+			message: 'Un nouvelle utilisateur a été crée',
+			result,
+		})
+	} catch (error) {
+		// console.log("erreur lors de la création de l'utilisateur", error);
+		res.status(400).json({
+			message: error.message,
+		})
+	}
+})
+
+// profil utilisateur
+router.get('/user', checkToken, async (req, res) => {
+	const _id = req.userId
+	const userProfil = await getUserById(_id)
+	res.json({
+		user: userProfil,
+	})
+})
 
 // connexion d'un utilisateur
-router.post("/login", async (req, res) => {
-  console.log(" info login : ", req.body);
-  const { email, password } = req.body;
+router.post('/login', async (req, res) => {
+	console.log(' info login : ', req.body)
+	const { email, password } = req.body
 
-  if (!email || !password) {
-    return res.status(401).json({
-      status: "error",
-      message: "Les informations saisies sont incorrectes",
-    });
-  }
+	if (!email || !password) {
+		return res.status(401).json({
+			status: 'error',
+			message: 'Les informations saisies sont incorrectes',
+		})
+	}
 
-  const user = await getUserByEmail(email);
+	const user = await getUserByEmail(email)
 
-  const pwdCompare = user && user._id ? user.password : null;
+	const pwdCompare = user && user._id ? user.password : null
 
-  if (!pwdCompare)
-    return res.status(401).json({
-      status: "error",
-      message: "vos identifiants sont incorrectes",
-    });
+	if (!pwdCompare)
+		return res.status(401).json({
+			status: 'error',
+			message: 'vos identifiants sont incorrectes',
+		})
 
-  const result = await comparePassword(password, pwdCompare);
-  if (!result) {
-    return res.status(401).json({
-      status: "error",
-      message: "vos identifiants sont incorrectes",
-    });
-  }
+	const result = await comparePassword(password, pwdCompare)
+	if (!result) {
+		return res.status(401).json({
+			status: 'error',
+			message: 'vos identifiants sont incorrectes',
+		})
+	}
 
-  const accessToken = await createAccessToken(user.email, `${user._id}`);
-  const refreshToken = await createRefreshToken(user.email, `${user._id}`);
+	const accessToken = await createAccessToken(user.email, `${user._id}`)
+	const refreshToken = await createRefreshToken(user.email, `${user._id}`)
 
-  res.status(200).json({
-    status: "success",
-    message: "Connexion réussie",
-    accessToken,
-    refreshToken,
-  });
-});
+	res.status(200).json({
+		status: 'success',
+		message: 'Connexion réussie',
+		accessToken,
+		refreshToken,
+	})
+})
 
-module.exports = router;
+module.exports = router
