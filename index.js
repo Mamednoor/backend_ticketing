a601991903f4916b41db7211ddff39135e564257
Mamednoor <mamed.gohabur@gmail.com>
2022-02-05 12:31:24 +0100

    [REFACTORING] refactoring DB connexion on conf.js file and comment

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const helmet = require('helmet')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const routes = require('./routes/route')
+const connectDB = require ('./conf')
 
 const PORT = process.env.PORT || 8080
 // sécurité pour l'API
@@ -17,30 +18,33 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(bodyParser.json()) // for parsing application/json
 
+// //connexion à MongoDB
+// mongoose.connect(process.env.DB_URL, {
+// 	auth: {
+// 		username: process.env.DB_USER,
+// 		password: process.env.DB_PASS,
+// 	},
+// 	useUnifiedTopology: true,
+// 	useNewUrlParser: true,
+// })
+
+// // affiche un message si la connexion à MongoDB est active ou un message d'erreur ainsi que la gestion des connexions
+// if (process.env.NODE_ENV !== 'production') {
+// 	const mDbConnect = mongoose.connection
+// 	mDbConnect.on('open', () => {
+// 		console.log('mDbConnect : ', 'MongoDB is connected')
+// 	})
+// 	mDbConnect.on('error', (error) => {
+// 		console.log('error mongoose', error)
+// 	})
+// 	// gestion des connections
+// 	app.use(morgan('dev'))
+// }
+
 //connexion à MongoDB
-mongoose.connect(process.env.DB_URL, {
-	auth: {
-		username: process.env.DB_USER,
-		password: process.env.DB_PASS,
-	},
-	useUnifiedTopology: true,
-	useNewUrlParser: true,
-})
+connectDB;
+app.use(morgan('dev'))
 
-//mongoose.connect(process.env.DB, { useNewUrlParser: true })
-
-// affiche un message si la connexion à MongoDB est active ou un message d'erreur ainsi que la gestion des connexions
-if (process.env.NODE_ENV !== 'production') {
-	const mDbConnect = mongoose.connection
-	mDbConnect.on('open', () => {
-		console.log('mDbConnect : ', 'MongoDB is connected')
-	})
-	mDbConnect.on('error', (error) => {
-		console.log('error mongoose', error)
-	})
-	// gestion des connections
-	app.use(morgan('dev'))
-}
 
 //appel les routes dans un fichier commun
 app.use('/', routes.api)
